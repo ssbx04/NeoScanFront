4cb0a0dbdb64a1bb4df6c3c913e09dd1caa344b0
ssbx04 <cheikhsad700@gmail.com>
2025-05-06 21:27:03 +0000

    Creation vues, ajout des routes, ajout font nexa et gestion de l'espacement(padding) global

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { ChatbotComponent } from './views/chatbot/chatbot.component';
+import { ResultatComponent } from './views/resultat/resultat.component';
+import { ScannerComponent } from './views/scanner/scanner.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component : HomeComponent
+  },
+  {
+      path: 'chatbot',
+      component: ChatbotComponent,
+  },
+  {
+      path: 'resultat',
+      component: ResultatComponent,
+  },
+  {
+    path: 'scanner',
+    component: ScannerComponent
+  },
+  {
+      path: '**',
+      redirectTo: ''
+  }
+];
