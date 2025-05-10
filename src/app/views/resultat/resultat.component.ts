aacbddaafaee26ef226bc3fb74738d6bbb82d316
ssbx04 <cheikhsad700@gmail.com>
2025-05-10 19:16:02 +0000

    Result done

--- src/app/views/resultat/resultat.component.ts
+++ src/app/views/resultat/resultat.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PredictionService } from '../../services/prediction.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-resultat',
@@ -8,5 +10,16 @@ import { Component } from '@angular/core';
   styleUrl: './resultat.component.css'
 })
 export class ResultatComponent {
-
+  constructor(
+      private readonly predictionService : PredictionService,
+      private readonly router: ActivatedRoute
+    ){}
+  base64String : string = ''
+  fileType : string = ''
+  reponse : string = ''
+  ngOnInit(){
+      this.base64String = sessionStorage.getItem('base64String') ?? '';
+      this.fileType = sessionStorage.getItem('fileType') ?? '';
+      this.reponse = sessionStorage.getItem('reponse') ?? '';
+  }
 }
