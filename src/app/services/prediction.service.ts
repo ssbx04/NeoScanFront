581cd2fddba0743658633f749a30cf7d51cd56ab
ssbx04 <cheikhsad700@gmail.com>
2025-05-10 19:19:19 +0000

    Scanner done

--- src/app/services/prediction.service.ts
+++ src/app/services/prediction.service.ts
@@ -1,9 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PredictionService {
 
-  constructor() { }
+  constructor(private readonly httpClient : HttpClient) { }
+  predictCancer(body : any) : Observable<any>{
+    return this.httpClient.post('http://127.0.0.1:8000/api/predict/',body)
+  }
 }
--- src/app/views/scanner/scanner.component.ts
+++ src/app/views/scanner/scanner.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PredictionService } from '../../services/prediction.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-scanner',
@@ -8,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrl: './scanner.component.css'
 })
 export class ScannerComponent {
+  constructor(
+    private readonly predictionService : PredictionService,
+    private readonly router: Router
+  ){}
+  image_selection_state : boolean = false
+  base64String : string = ''
+  fileType : string = ''
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
 
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+
+    const file = input.files[0];
+
+    this.fileType = file.type;
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.base64String = (reader.result as string).split(',')[1];
+    };
+    reader.readAsDataURL(file);
+    this.image_selection_state = true
+  }
+  predictCancer(){
+    this.predictionService.predictCancer({
+      "image": this.base64String
+    }).subscribe(response =>{
+      this.saveDataToStorage(this.base64String,this.fileType,response.predicted_class)
+      this.router.navigate(['/resultat']);
+  })
+  }
+  saveDataToStorage(base64String: string, fileType: string,reponse : string) {
+    sessionStorage.setItem('base64String', base64String);
+    sessionStorage.setItem('fileType', fileType);
+    sessionStorage.setItem('reponse', reponse);
+  }
 }
